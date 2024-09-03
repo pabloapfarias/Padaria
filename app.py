222109e204e20f26edd4764257968d823a9fd574
Pablo Farias <contato@pablofarias.com.br>
2024-09-02 23:24:34 -0300

    Criação do banco de dados sqlite

--- app.py
+++ app.py
@@ -8,6 +8,23 @@ db.init_app(app)
 
 
 
+class Product(db.Model):
+    __tablename__ = 'products'
+    id = db.Column(db.Integer, primary_key=True)
+    nome =db.Column(db.String(100), nullable=False)
+    descricao =db.Column(db.String(500))
+    ingredientes =db.Column(db.String(500))
+    origen = db.Column(db.String(100))
+    imagem =db.Column(db.String(100))
+
+    def __init__(self, nome: str, descricao: str,ingredientes: str,  origen: str, imagem: str) -> None:
+        self.nome = nome
+        self.descricao = descricao
+        self.ingredientes = ingredientes
+        self.origen = origen
+        self.imagem = imagem
+
+
 @app.route('/')
 def hello_world():  # put application's code here
     return 'Hello World!'
