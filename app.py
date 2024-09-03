300250491667034225f40fe18afdaafa997493ea
Pablo Farias <contato@pablofarias.com.br>
2024-09-03 01:12:52 -0300

    Formação de lista de produtos

--- app.py
+++ app.py
@@ -15,20 +15,33 @@ class Product(db.Model):
     nome =db.Column(db.String(100), nullable=False)
     descricao =db.Column(db.String(500))
     ingredientes =db.Column(db.String(500))
-    origen = db.Column(db.String(100))
+    origem = db.Column(db.String(100))
     imagem =db.Column(db.String(100))
 
-    def __init__(self, nome: str, descricao: str,ingredientes: str,  origen: str, imagem: str) -> None:
+    def __init__(self, nome: str, descricao: str,ingredientes: str,  origem: str, imagem: str) -> None:
         self.nome = nome
         self.descricao = descricao
         self.ingredientes = ingredientes
-        self.origen = origen
+        self.origen = origem
         self.imagem = imagem
 
 
 @app.route('/')
-def hello_world():  # put application's code here
+def home():  # put application's code here
     return render_template('index.html')
+
+@app.route('/listar_produtos')
+def listar_produtos():
+    produtos = db.session.execute(db.select(Product)).scalars()
+    return render_template('listar_produtos.html', produtos=produtos)
+    print(produtos)
+
+
+
+@app.route('/cadastrar_produtos')
+def cadastrar_produtos():  # put application's code here
+    return render_template('cadastrar.html')
+
 
 
 if __name__ == '__main__':
